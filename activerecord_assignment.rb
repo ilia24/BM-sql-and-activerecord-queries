63a3721c180a3af004b9e4f08eb6f7938ccfb922
ilia24 <iliademertchian24@gmail.com>
2017-04-23 02:08:10 -0400

    Complete questions

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -21,11 +21,11 @@ Track.all.order(unit_price: :desc)
 
 
 # 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
-
+Track.order(unit_price: :asc).limit(1).select('name')
 
 
 # 4) Find the 2 oldest artists.
-
+Track.order(created_at: :asc).limit(1)
 
 
 ### Stretch Exercise (Active Record Query Interface)
